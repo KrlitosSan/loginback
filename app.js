673b7d9079d1d9b0e530648df941c151d8535c1a
krlitos <krlitos.san.ks@gmail.com>
2025-03-11 19:56:47 -0500

    Base de datos

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const mysql = require("mysql2/promise");
 
 // Create the connection pool. The pool-specific settings are the defaults
 const pool = mysql.createPool({
-  host: "localhost",
+  host: "switchback.proxy.rlwy.net",
   user: "root",
-  database: "notariabd",
+  database: "login",
+  password: "KuvudZPiNkSfDvaViYvPAawNlkfLprQn",
+  port: 12844,
 });
 // variable para guardar usuarios y contraseñas y ruta /login para el inicio de sesion
 
